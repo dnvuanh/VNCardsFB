a8b6aa27cea01ddb333f599c0492c5784144c98b
Viet <nviet89@live.com>
2019-02-01 18:50:49 +0700

    + Add parse set of cards function

--- servercode/rtScript/Kill13.js
+++ servercode/rtScript/Kill13.js
@@ -225,18 +225,15 @@ var OnStartGame = function(firstId)
 {
     var CARD_NUM = 52;
     var CARD_PER_PLAYER = 13;
-    var deck = new Array(CARD_NUM);
-    //reload deck
-    for(var i = 0; i < CARD_NUM; i++) {
-        deck[i] = i;
-    }
+    var deck = Define.DefaultCards.slice();
     //shuffle
     for(var i = CARD_NUM; i > 1; i--) {
         var randomIdx = Math.random() * i | 0;
+        
+        //deck[i - 1, randomIdx] = deck[randomIdx, i - 1];
         var temp = deck[i - 1];
         deck[i - 1] = deck[randomIdx];
         deck[randomIdx] = temp;
-        //deck[i - 1, randomIdx] = deck[randomIdx, i - 1];
     }
     
     //send cards to players
@@ -249,6 +246,100 @@ var OnStartGame = function(firstId)
         SendCardsToPlayer(player.getPlayerId(), player.getPeerId());
     }
 }
+
+/*++++++++++++++++++++++++++++++++++++++ PARSE CARD ++++++++++++++++++++++++++++++++++++++++++++++*/
+var cardValue = function(card) 
+{
+    return card / 4 | 0;
+}
+
+var isSame = function(cards)
+{
+    if(cards.length < 2) {
+        return false;
+    }
+
+    for(var i = 1; i < cards.length; i++) {
+        if(cardValue(cards[0]) != cardValue(cards[i])) {
+                return false;
+            }
+    }
+    return true;
+}
+
+var isStraight = function(cards)
+{
+    if(cards.length < 3) {
+        return false;
+    }
+
+    for(var i = 0; i < cards.length - 1; i++) {
+        if(cardValue(cards[i]) - cardValue(cards[i+1]) != 1){
+            return false;
+        }
+    }
+    return true;
+}
+
+var compareGreater = function(a, b) 
+{ 
+    return b - a;
+}
+
+var parseCards = function(cards) 
+{  
+    cards.sort(compareGreater);
+    var result = {setType: Define.SetType.ERROR, numOfCard:"", topCard: ""};
+    result.numOfCard = cards.length;
+    result.topCard = cards[0];
+
+    if(isStraight(cards))
+    {
+        result.setType = Define.SetType.STRAIGHT;
+        return result;
+    }
+    
+    switch (cards.length) {
+    case 1:
+        result.setType = Define.SetType.SINGLE;
+        break;
+    case 2:
+        if(isSame(cards)) {
+            result.setType = Define.SetType.PAIR;
+        }
+        break;
+    case 3:
+        if(isSame(cards)) {
+            result.setType = Define.SetType.TRIPLE;
+        }
+        break;
+    case 4:
+        if(isSame(cards)) {
+            result.setType = Define.SetType.QUADS;
+        }
+        break;
+    case 6:
+        if(isSame([cards[0], cards[1]]) &&
+            isSame([cards[2], cards[3]]) &&
+            isSame([cards[4], cards[5]]) &&
+            isStraight([cards[0], cards[2], cards[4]])) {
+                result.setType = Define.SetType.THREEPAIRS;
+            }
+        break;
+    case 8:
+        if(isSame([cards[0], cards[1]]) &&
+            isSame([cards[2], cards[3]]) &&
+            isSame([cards[4], cards[5]]) &&
+            isSame([cards[6], cards[7]]) &&
+            isStraight([cards[0], cards[2], cards[4], cards[6]])) {
+                result.setType = Define.SetType.FOURPAIRS;
+            }
+        break;
+    default:
+    }
+    return result;
+}
+/*-------------------------------------- PARSE CARD ---------------------------------------------*/
 
 var main = function(){
     RTSession.onPacket(ServerCode.RQ_LOAD_MATCH, OnRequestMatch.bind(this));
